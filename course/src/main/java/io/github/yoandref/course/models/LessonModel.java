a63d2e7f1feff6a014221bf8d79fc4957be3061b
devandref <andredepaulacontato@gmail.com>
2026-04-12 12:01:45 -0300

    hotifix: bug     @Fetch(FetchMode.SUBSELECT)  para     @Fetch(FetchMode.JOIN)

--- course/src/main/java/io/github/yoandref/course/models/LessonModel.java
+++ course/src/main/java/io/github/yoandref/course/models/LessonModel.java
@@ -37,7 +37,7 @@ public class LessonModel implements Serializable {
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm:ss")
     private LocalDateTime creationDate;
 
-    @Fetch(FetchMode.SUBSELECT)
+    @Fetch(FetchMode.JOIN)
     @ManyToOne(optional = false, fetch = FetchType.LAZY)
     @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private ModuleModel module;
