0117467ebf2e5247bae2429f62af5e0880980fb0
devandref <andredepaulacontato@gmail.com>
2026-04-13 06:23:48 -0300

    include: controller para module

--- course/src/main/java/io/github/yoandref/course/services/ModuleService.java
+++ course/src/main/java/io/github/yoandref/course/services/ModuleService.java
@@ -2,8 +2,12 @@ package io.github.yoandref.course.services;
 
 import io.github.yoandref.course.models.ModuleModel;
 
+import java.util.Optional;
+import java.util.UUID;
+
 public interface ModuleService {
 
     void delete(ModuleModel moduleModel);
-
+    ModuleModel save(ModuleModel moduleModel);
+    Optional<ModuleModel> findModuleIntoCourse(UUID courseId, UUID moduleId);
 }
