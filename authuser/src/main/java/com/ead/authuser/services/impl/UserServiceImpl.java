0f4005459c85bea95fe238c43130b1bb316535b2
devandref <andredepaulacontato@gmail.com>
2026-04-28 17:27:59 -0300

    .

---
+++ authuser/src/main/java/com/ead/authuser/services/impl/UserServiceImpl.java
@@ -0,0 +1,67 @@
+package com.ead.authuser.services.impl;
+
+import com.ead.authuser.models.UserCourseModel;
+import com.ead.authuser.models.UserModel;
+import com.ead.authuser.repositories.UserCourseRepository;
+import com.ead.authuser.repositories.UserRepository;
+import com.ead.authuser.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    UserCourseRepository userCourseRepository;
+
+    @Override
+    public List<UserModel> findAll() {
+        return userRepository.findAll();
+    }
+
+    @Override
+    public Optional<UserModel> findById(UUID userId) {
+        return userRepository.findById(userId);
+    }
+
+    @Transactional
+    @Override
+    public void delete(UserModel userModel) {
+        List<UserCourseModel> userCourseModelList = userCourseRepository.findAllUserCourseIntoUser(userModel.getUserId());
+        if(!userCourseModelList.isEmpty()){
+            userCourseRepository.deleteAll(userCourseModelList);
+        }
+        userRepository.delete(userModel);
+    }
+
+    @Override
+    public UserModel save(UserModel userModel) {
+        return userRepository.save(userModel);
+    }
+
+    @Override
+    public boolean existsByUsername(String username) {
+        return userRepository.existsByUsername(username);
+    }
+
+    @Override
+    public boolean existsByEmail(String email) {
+        return userRepository.existsByEmail(email);
+    }
+
+    @Override
+    public Page<UserModel> findAll(Specification<UserModel> spec, Pageable pageable) {
+        return userRepository.findAll(spec, pageable);
+    }
+}
